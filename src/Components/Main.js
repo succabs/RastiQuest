7e56f48d5c94a98a893aab3b4ae8757e94221a59
Arttu <artnikkila@gmail.com>
2022-11-05 14:11:51 +0200

    added photos, updated main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import map from '../Photos/map1.jpg';
 
 export default function Navbar() {
     return (
         <div className="container-fluid">
-            <p>RastiQuest</p>
+            <h1>RastiQuest</h1>
+            <img src={map} alt="map"/>
+
         </div>
         );
 }
